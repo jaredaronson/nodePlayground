79d9d617fa4be7e760a7298f375ecd757e2019d5
Jared Aronson <jared@madhousetees.com>
2018-02-07 20:31:11 -0500

    modify server.js

--- server.js
+++ server.js
@@ -6,8 +6,4 @@ var server = app.listen(3000);
 
 app.use(express.static('public'));
 
-<<<<<<< HEAD
 console.log("My socket server is running");
-=======
-console.log("My socket server is running");
->>>>>>> f3b66d983e7697b0766071d826c8783140d7efe3
