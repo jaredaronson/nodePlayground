f780c100f41614eb69bbe79cfb28ba70f0b1f0bc
Jared Aronson <jared@madhousetees.com>
2018-03-13 13:23:31 -0400

    can now midify firebase database

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var hbs = require('hbs');
+var bodyParser = require('body-parser');
+
+/////////////////////////////////////////////
+var admin = require('firebase-admin');
+var serviceAccount = require('./test-database-ccc1c-firebase-adminsdk-n28nx-f2c652b48f.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://test-database-ccc1c.firebaseio.com/'
+});
+
+var db = admin.database();
+var ref = db.ref("switches");
+var usersRef = ref.child("first");
+/////////////////////////////////////////////
 
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -15,24 +32,56 @@ app.listen(PORT, function(){
 
 });
 
-
 app.get('/', function(req, res){
 
     res.render('index.hbs',{
 
         pageTitle: 'Index Page',
         currentYear : new Date().getFullYear()
+
     });
 
 });
 
 app.get('/home', function(req, res){
-    //var test = req.params.test;
+
     res.render('home.hbs',{
 
         pageTitle: 'Home Page',
         currentYear : new Date().getFullYear(),
 
     });
+
+});
+
+app.get('/posttest', function(req, res){
+
+    res.render('posttest.hbs');
+
+});
+
+app.post('/posttest', urlencodedParser, function(req, res){
+
+  console.log(req.body.test);
+
+  if(req.body.test == 'true'){
+
+    usersRef.update({
+      pressed: true
+    });
+
+  }else if(req.body.test == 'false'){
+
+    usersRef.update({
+      pressed: false
+    });
+
+  }else{
+
+    console.log("Invalid entry");
+
+  }
+
+  res.render('posttest.hbs');
 
 });
